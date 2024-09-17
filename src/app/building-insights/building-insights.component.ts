9a185ff09ecee72c99ae93fc823c61f547268a72
Chaitanya, Allamsetti <C918987@exelonds.com>
2024-09-17 20:21:07 +0530

    added solar potential

--- src/app/building-insights/building-insights.component.ts
+++ src/app/building-insights/building-insights.component.ts
@@ -4,8 +4,8 @@ import { BuildingInsightsService } from '../shared/services/building-insights.se
 import { CommonModule, JsonPipe } from '@angular/common';
 import { createPalette, normalize, rgbToColor } from './../shared/utils/visualize';
 import { panelsPalette } from './../shared/utils/colors';
-import { FormsModule } from '@angular/forms'; 
-import { SliderModule } from 'primeng/slider'; 
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
 import { HlmH2Directive } from '@spartan-ng/ui-typography-helm';
 import { ChartData, ChartOptions } from 'chart.js';
 import { ChartModule } from 'primeng/chart';
@@ -20,6 +20,7 @@ import {
   HlmThComponent,
   HlmTrowComponent,
 } from '@spartan-ng/ui-table-helm';
+import { Router } from '@angular/router';
 
 
 
@@ -40,7 +41,7 @@ export class BuildingInsightsComponent implements OnInit {
   configId!: number;
   panelCapacityWatts!: number;
   showPanels  = true;
-
+  showLoader = true;
   @Input('googleMapsApiKey') googleMapsApiKey!: string;
   @Input('geometryLibrary') geometryLibrary!: google.maps.GeometryLibrary;
   @Input('location') location!: google.maps.LatLng;
@@ -67,12 +68,27 @@ export class BuildingInsightsComponent implements OnInit {
   energyCostPerKwhInput = 0.31;
   dcToAcDerateInput = 0.85;
   // Slider settings
-  panelCount: number = 30; // Default value for the slider
-
+  panelCount: number = 31; // Default value for the slider
+  yearlyEnergyDcKwh = 12000;
+  efficiencyDepreciationFactor = 0.995;
+  installationLifeSpan = 20;
+  costIncreaseFactor = 1.022;
+  discountRate = 1.04;
   // Find the config that covers the yearly energy consumption.
   yearlyKwhEnergyConsumption!: number;
-  
-  constructor(private buildingInsightsService: BuildingInsightsService ) { }
+  installationSizeKw!: number;
+  installationCostTotal!: number;
+  yearlyEnergy!: number;
+  initialAcKwhPerYear!: number;
+  yearlyProductionAcKwh!: number[];
+  yearlyUtilityBillEstimates!: number[];
+  remainingLifetimeUtilityBill!: number;
+  totalCostWithSolar!: number;
+  totalCostWithoutSolar!: number;
+  yearlyCostWithoutSolar!: number[];
+  showErrorLayer: boolean = false;
+
+  constructor(private buildingInsightsService: BuildingInsightsService, private route: Router) { }
 
   ngOnInit() {
     // Automatically call the function to set panels when the component initializes
@@ -118,32 +134,128 @@ export class BuildingInsightsComponent implements OnInit {
         }
       }
     };
-  
 
-    
+
+
   }
 
   ngOnChanges() {
     if (this.location) {
-      // console.log(this.location.lat(), this.location.lng());
+      this.buildingInsightsService.findClosestBuilding(this.location, this.googleMapsApiKey).subscribe(res => {
+        this.buildingInsights = res;
+        this.showLoader = false;
+        if (!this.configId) {
+          this.showErrorLayer = false;
+          console.log(this.panelCount);
+          console.log(this.panelCapacityWatts);
+          this.installationSizeKw = this.panelCount * this.panelCapacityWattsInput / 1000;
+          this.installationCostTotal = this.installationCostPerWatt * this.installationSizeKw * 1000;
+          console.log("Installation size",this.installationSizeKw);
+          console.log("Installation cost",this.installationCostTotal);
+          this.defaultPanelCapacity = this.buildingInsights.solarPotential.panelCapacityWatts;
+          this.yearlyKwhEnergyConsumption = (this.monthlyAverageEnergyBillInput / this.energyCostPerKwhInput) * 12;
+          console.log("Yearly Energy",this.yearlyKwhEnergyConsumption);
+          this.panelCapacityRatio = this.panelCapacityWattsInput / this.defaultPanelCapacity;
+          this.configId = this.findSolarConfig(this.buildingInsights.solarPotential.solarPanelConfigs, this.yearlyKwhEnergyConsumption, this.panelCapacityRatio, this.dcToAcDerateInput);
+          this.yearlyEnergy = (this.buildingInsights.solarPotential.solarPanelConfigs[this.configId]?.yearlyEnergyDcKwh ?? 0) * this.panelCapacityRatio;
+          this.setPanelConfig(this.configId);
+          console.log(this.configId);
+          this.initialAcKwhPerYear = this.yearlyEnergyDcKwh * this.dcToAcDerateInput;
+          this.yearlyProductionAcKwh = [...Array(this.installationLifeSpan).keys()].map(
+            (year) => this.initialAcKwhPerYear * this.efficiencyDepreciationFactor ** year
+          );
+          this.yearlyUtilityBillEstimates = this.yearlyProductionAcKwh.map(
+            (yearlyKwhEnergyProduced, year) => {
+              const billEnergyKwh = this.yearlyKwhEnergyConsumption - yearlyKwhEnergyProduced;
+              const billEstimate = (billEnergyKwh * this.energyCostPerKwhInput * this.costIncreaseFactor ** year) / this.discountRate ** year;
+              return Math.max(billEstimate, 0);
+            }
+          )
+
+          this.remainingLifetimeUtilityBill = this.yearlyUtilityBillEstimates.reduce((x, y) => x + y, 0);
+          this.totalCostWithSolar = this.installationCostTotal + this.remainingLifetimeUtilityBill - this.solarIncentives;
+          this.yearlyCostWithoutSolar = [...Array(this.installationLifeSpan).keys()].map(
+            (year) =>
+            (this.monthlyAverageEnergyBillInput * 12 * this.costIncreaseFactor ** year) / this.discountRate ** year,
+          );
+          this.totalCostWithoutSolar = this.yearlyCostWithoutSolar.reduce((x, y) => x + y, 0);
+          console.log("Total Cost with solar",this.totalCostWithSolar);
+          console.log("Total cost without solar", this.totalCostWithoutSolar);
+
+          this.setSolarPanels();
+        }
+      }, err => {
+        console.error('GET buildingInsights error\n', err);
+        this.showErrorLayer = true;
+        this.showLoader = false;
+        this.requestError = err.error;
+      });
+    }
+  }
+
+  updateCalculations() {
+    if (this.location) {
       this.buildingInsightsService.findClosestBuilding(this.location, this.googleMapsApiKey).subscribe(res => {
         this.buildingInsights = res;
         console.log(this.buildingInsights);
-        if (!this.configId) {
-          this.defaultPanelCapacity = this.buildingInsights.solarPotential.panelCapacityWatts;
-          this.yearlyKwhEnergyConsumption = (this.monthlyAverageEnergyBillInput / this.energyCostPerKwhInput) * 12;
-          this.panelCapacityRatio = this.panelCapacityWattsInput / this.defaultPanelCapacity;
-          this.configId = this.findSolarConfig(this.buildingInsights.solarPotential.solarPanelConfigs, this.yearlyKwhEnergyConsumption, this.panelCapacityRatio, this.dcToAcDerateInput);
-          this.setPanelConfig(this.configId);
+        console.log(this.configId);
+          console.log("inside it");
 
+          this.installationSizeKw = this.panelCount * this.panelCapacityWattsInput / 1000;
+          console.log("Installation Size", this.installationSizeKw);
+          this.installationCostTotal = this.installationCostPerWatt * this.installationSizeKw * 1000;
+          console.log("Installation Cost", this.installationCostTotal);
+          this.yearlyKwhEnergyConsumption = (this.monthlyAverageEnergyBillInput / this.energyCostPerKwhInput) * 12;
+          console.log("MonthlyBill",this.monthlyAverageEnergyBillInput)
+          console.log("Yearly Energy",this.yearlyKwhEnergyConsumption);
+          this.defaultPanelCapacity = this.buildingInsights.solarPotential.panelCapacityWatts;
+          this.panelCapacityRatio = this.panelCapacityWattsInput / this.defaultPanelCapacity;
+
+            this.configId = this.findSolarConfig(this.buildingInsights.solarPotential.solarPanelConfigs, this.yearlyKwhEnergyConsumption, this.panelCapacityRatio, this.dcToAcDerateInput);
+          // this.setPanelConfig(this.configId);
+          // console.log(this.configId);
+          this.yearlyEnergy = (this.buildingInsights.solarPotential.solarPanelConfigs[this.configId]?.yearlyEnergyDcKwh ?? 0) * this.panelCapacityRatio;
+          this.panelCount = this.buildingInsights.solarPotential.solarPanelConfigs[this.configId].panelsCount;
+          this.initialAcKwhPerYear = this.yearlyEnergyDcKwh * this.dcToAcDerateInput;
+          this.yearlyProductionAcKwh = [...Array(this.installationLifeSpan).keys()].map(
+            (year) => this.initialAcKwhPerYear * this.efficiencyDepreciationFactor ** year
+          );
+          this.yearlyUtilityBillEstimates = this.yearlyProductionAcKwh.map(
+            (yearlyKwhEnergyProduced, year) => {
+              const billEnergyKwh = this.yearlyKwhEnergyConsumption - yearlyKwhEnergyProduced;
+              const billEstimate = (billEnergyKwh * this.energyCostPerKwhInput * this.costIncreaseFactor ** year) / this.discountRate ** year;
+              return Math.max(billEstimate, 0);
+            }
+          )
+          this.yearlyCostWithoutSolar = [...Array(this.installationLifeSpan).keys()].map(
+            (year) =>
+              (this.monthlyAverageEnergyBillInput * 12 * this.costIncreaseFactor ** year) / this.discountRate ** year,
+          );
+          this.totalCostWithoutSolar = this.yearlyCostWithoutSolar.reduce((x, y) => x + y, 0);
+          this.remainingLifetimeUtilityBill = this.yearlyUtilityBillEstimates.reduce((x, y) => x + y, 0);
+          console.log("Installation cost", this.installationCostTotal);
+          console.log("Remaining Life time utility bill", this.remainingLifetimeUtilityBill);
+          console.log("Solar Incentives", this.solarIncentives);
+          this.totalCostWithSolar = this.installationCostTotal + this.remainingLifetimeUtilityBill - this.solarIncentives;
+          this.yearlyCostWithoutSolar = [...Array(this.installationLifeSpan).keys()].map(
+            (year) =>
+            (this.monthlyAverageEnergyBillInput * 12 * this.costIncreaseFactor ** year) / this.discountRate ** year,
+          );
+          this.totalCostWithoutSolar = this.yearlyCostWithoutSolar.reduce((x, y) => x + y, 0);
+          console.log("Total Cost with solar after",this.totalCostWithSolar);
+          console.log("Total cost without solar after", this.totalCostWithoutSolar);
           // Automatically set panels after getting the response
           this.setSolarPanels();
-        }
       }, err => {
         console.error('GET buildingInsights error\n', err);
         this.requestError = err.error;
       });
     }
+    this.setSolarPanels();
+  }
+
+  goBack() {
+    window.location.reload();
   }
 
   findSolarConfig(solarPanelConfigs: SolarPanelConfig[], yearlyKwhEnergyConsumption: number, panelCapacityRatio: number, dcToAcDerateInput: number) {
@@ -157,20 +269,20 @@ export class BuildingInsightsComponent implements OnInit {
     this.configId = configId;
     this.panelConfig = this.buildingInsights.solarPotential.solarPanelConfigs[this.configId];
   }
-  
+
 
   setSolarPanels() {
     if (!this.buildingInsights || !this.buildingInsights.solarPotential) return;
-  
+
     // Clear any existing panels from the map
     this.solarPanels.forEach((panel) => panel.setMap(null));
     this.solarPanels = [];
-  
+
     const solarPotential = this.buildingInsights.solarPotential;
     const palette = createPalette(panelsPalette).map(rgbToColor);
     const minEnergy = solarPotential.solarPanels.slice(-1)[0].yearlyEnergyDcKwh;
     const maxEnergy = solarPotential.solarPanels[0].yearlyEnergyDcKwh;
-  
+
     // Create solar panels based on the slider value (panelCount)
     this.solarPanels = solarPotential.solarPanels.slice(0, this.panelCount).map((panel: any) => {
       const [w, h] = [solarPotential.panelWidthMeters / 2, solarPotential.panelHeightMeters / 2];
@@ -181,11 +293,11 @@ export class BuildingInsightsComponent implements OnInit {
         { x: -w, y: +h }, // top left
         { x: +w, y: +h }, // top right again to close the polygon
       ];
-  
+
       const orientation = panel.orientation === 'PORTRAIT' ? 90 : 0;
       const azimuth = solarPotential.roofSegmentStats[panel.segmentIndex].azimuthDegrees;
       const colorIndex = Math.round(normalize(panel.yearlyEnergyDcKwh, maxEnergy, minEnergy) * 255);
-  
+
       return new google.maps.Polygon({
         paths: points.map(({ x, y }) =>
           this.geometryLibrary.spherical.computeOffset(
@@ -201,7 +313,7 @@ export class BuildingInsightsComponent implements OnInit {
         fillOpacity: 0.9,
       });
     });
-  
+
     // Add the newly created solar panels to the map based on the slider's value
     this.solarPanels.forEach((panel, i) => {
       if (this.showPanels && this.panelConfig && i < this.panelCount) {
